360f5da9ef8051b0075116fade6787b3971e9c60
masonsluder <sharppened@gmail.com>
2024-09-29 17:38:21 -0400

    Started Section 3 (Team)

--- NetworkServer.cpp
+++ NetworkServer.cpp
@@ -7,18 +7,21 @@
 #include <chrono>
 #include <thread>
 #include <unordered_map>
+#include "networkthread.h"
+#include <mutex>
 
-//#include "zhelpers.hpp"
-
-int clientIteration = 0;
-int clientIdentifierCounter = 0;
 
 typedef struct {
     int m_id;
     int m_iteration;
+    char m_identifier[9];
+    std::thread m_thread;
 } ClientIteration;
 
 int main(int argc, char* argv[]) {
+
+	int clientIteration = 0;
+	int clientIdentifierCounter = 0;
 
     // Initialize list of Client Iteration trackers
     std::vector<ClientIteration> clientList = std::vector<ClientIteration>();
@@ -38,12 +41,6 @@ int main(int argc, char* argv[]) {
 
     // Send out multipart messages forever
     while (true) {
-        const Uint8* keyboardState = SDL_GetKeyboardState(NULL);
-        if (keyboardState[SDL_SCANCODE_ESCAPE]) {
-            SDL_Quit();  // Clean up SDL
-            zmq_ctx_destroy((void*)context);
-            exit(0);
-        }
 
         // REQ model ready to receive requests from the client.
         zmq::message_t clientIdRequest;
@@ -59,39 +56,58 @@ int main(int argc, char* argv[]) {
             clientIdentifierCounter++;
             std::cout << "Client [" << clientIdentifierCounter << "] connected.\n";
 
-            // Add new client to list of clients
-            clientList.push_back(ClientIteration() = { clientIdentifierCounter, 0 });
+            // Cast clientIdentifierCounter to a char array to send to client
+            char clientIdentifier[9] = "Client_";
+            clientIdentifier[7] = static_cast<char>(clientIdentifierCounter + 48);
+            clientIdentifier[8] = '\0';
 
+            // TODO Create new thread for newly connected client?
+            // Mutex to handle locking, condition variable to handle notifications between threads
+            std::mutex m;
+            std::condition_variable cv;
+
+            // Create thread objects
+            NetworkThread clientThread(0, NULL, &m, &cv);
+
+            // Start thread
+            std::thread first(&NetworkThread::run, &clientThread);
+
+            // Add new client to list of clients
+            ClientIteration newClient = { clientIdentifierCounter, 0, {}, std::move(std::thread(&NetworkThread::run, clientThread)) };
+            strncpy_s(newClient.m_identifier, clientIdentifier, sizeof(newClient.m_identifier));
+            clientList.push_back(newClient);
+            
             std::string clientString = std::to_string(clientIdentifierCounter);
 
-            // Send back to the client
-            zmq::message_t msg(clientString);
+            std::cout << "Client Identifier Initialized: " << clientIdentifier << "\n";
+
+            //assert("Client_" + std::to_string(clientIdentifierCounter) == "Client_1");
+
+            // Send the identifier back to the client
+            zmq::message_t msg("Client_" + std::to_string(clientIdentifierCounter));
             reply.send(msg, zmq::send_flags::none);
-
-            // TODO Create new thread for newly connected client
-
-            // Cast clientIdentifierCounter to a char array to send to client
-            char clientIdentifier[2];
-            clientIdentifier[0] = static_cast<char>(clientIdentifierCounter + 48);
-            clientIdentifier[1] = '\0';
-
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            // Handle publishing to clients (in their branch using each of their identifiers)
-            publisher.send(zmq::str_buffer(clientIdentifier), zmq::send_flags::sndmore);
-            publisher.send(zmq::str_buffer("Information to give to client"));
         }
 
         // Iterate throught the list of clients and broadcast each of their iteration numbers to each of the clients
         //std::cout << "Iterating through ClientIterations\n";
         for (ClientIteration& client : clientList) {
+            // Handle publishing to clients (in their branch using each of their identifiers)
             //std::cout << "Looping: " << client.m_id << "\n";
 			client.m_iteration += 1;
             // Creates stringstream for string building
             std::stringstream ss;
             // Prints out Client X: Iteration Y, then increments Y
-            ss << "Client " << client.m_id << ": " << "Iteration " << client.m_iteration << "\n";
+            ss << " " << client.m_id << " Iteration " << client.m_iteration;
+
+            //std::cout << client.m_identifier << ": " << "Iteration " << client.m_iteration << "\n";
             // Broadcast message to clients
             zmq::message_t iterationStr(ss.str());
+            
+            // Send to one client (FOR SECTION 5)
+            publisher.send(zmq::str_buffer(client.m_identifier), zmq::send_flags::sndmore);
+
+            // Send to all clients
+            //publisher.send(zmq::str_buffer("Client"), zmq::send_flags::sndmore);
             publisher.send(iterationStr, zmq::send_flags::dontwait);
         }
 
