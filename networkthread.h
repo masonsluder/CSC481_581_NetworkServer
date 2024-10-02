b88073b556a26e478e42b5cdde2bc1846ccfe36b
masonsluder <sharppened@gmail.com>
2024-10-02 18:08:05 -0400

    Starting Section 4

--- networkthread.h
+++ networkthread.h
@@ -8,11 +8,11 @@
 * https://moodle-courses2425.wolfware.ncsu.edu/pluginfile.php/499841/mod_resource/content/4/ThreadExample.cpp
 *
 */
-class NetworkThread
+class Thread
 {
     bool busy; // a member variable used to indicate thread "status"
     int i; // an identifier
-    NetworkThread* other; // a reference to the "other" thread in this example
+    Thread* other; // a reference to the "other" thread in this example
     std::mutex* _mutex; // the object for mutual exclusion of execution
     std::condition_variable* _condition_variable; // for messaging between threads
 
@@ -26,7 +26,7 @@ public:
     * @param _mutex: The mutex lock for the thread, this should use the mutex from the Timeline
     * @param _condition_variable: The condition variable for the mutex that controls when
     */
-    NetworkThread(int i, NetworkThread* other, std::mutex* _mutex, std::condition_variable* _condition_variable);
+    Thread(int i, Thread* other, std::mutex* _mutex, std::condition_variable* _condition_variable);
 
     /**
     * Locks the mutex, then sets thread to busy
