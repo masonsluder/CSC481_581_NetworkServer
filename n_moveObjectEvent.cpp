537b80db70c3aa47a19588e2d36b0a847ec0810a
masonsluder <sharppened@gmail.com>
2024-11-08 18:26:43 -0500

    Started Section 2.

---
+++ n_moveObjectEvent.cpp
@@ -0,0 +1,14 @@
+#include "n_moveObjectEvent.h"
+
+namespace N_Events{
+	/**
+	* Constructor for N_MoveObjectEvent
+	*/
+	N_MoveObjectEvent::N_MoveObjectEvent(std::vector<N_GameObject*> goRef, int64_t timeStampPriority, int priority, zmq::socket_t* socket) {
+		//this->
+	}
+
+	void N_MoveObjectEvent::onEvent() const {
+	
+	}
+}
--- n_moveObjectEvent.h
+++ n_moveObjectEvent.h
@@ -1,1 +1,31 @@
 #pragma once
+
+#ifndef NMOVEOBJECTEVENT_H
+#define NMOVEOBJECTEVENT_H
+
+
+#include "n_event.h"
+#include "networkVector2D.h"
+#include "n_GameObject.h"
+
+#include <zmq.hpp>
+
+namespace N_Events {
+	class N_MoveObjectEvent : public virtual N_Event {
+	private:
+
+	public:
+
+		/**
+		* Constructor for N_MoveObjectEvent
+		*/
+		N_MoveObjectEvent(std::vector<N_GameObject*> goRef, int64_t timeStampPriority, int priority, zmq::socket_t* socket);
+
+		void onEvent() const override;
+
+	};
+
+}
+
+
+#endif
