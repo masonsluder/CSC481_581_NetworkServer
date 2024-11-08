3a3fdbc55cbd450fb7979883b3295875817a3723
brianhhuynh38 <brianhhuynh38@gmail.com>
2024-11-08 15:33:43 -0500

    Starting transfer to networkServer

---
+++ n_event.cpp
@@ -0,0 +1,74 @@
+#include "n_event.h"
+#include "n_GameObject.h"
+
+namespace N_Events {
+
+	N_Event::N_Event() {
+		this->m_goRefVector = std::vector<N_GameObject*>();
+		this->m_timeStampPriority = 0;
+		this->m_priority = 0;
+	}
+
+	/**
+	* Constructor for the N_Event class
+	*
+	* @param goRef: A reference to the Gameobject that the N_Event will affect
+	* @param timeStampPriority: The timestamp at which the N_Event should be deployed
+	* @param priority: A secondary priority
+	*/
+	N_Event::N_Event(std::vector<N_GameObject*> goRefVector, int64_t timeStampPriority, int priority) {
+		this->m_goRefVector = std::vector<N_GameObject*>();
+		this->m_goRefVector = goRefVector;
+		this->m_timeStampPriority = timeStampPriority;
+		this->m_priority = priority;
+	}
+
+	/**
+	* Destructor for an N_Event that gets rid of the reference to N_GameObject
+	*/
+	N_Event::~N_Event() {
+		this->m_goRefVector.clear();
+	}
+
+	/**
+	* Returns a string in JSON format for the N_Event, N_GameObjectUUID is sent as 0 (invalid) if there is no reference
+	*/
+	void N_Event::to_json(json& j) {
+		// Add all fields to the json, but records N_GameObject ID as 0 if there is no N_GameObject reference
+		std::vector<int> idVector = std::vector<int>();
+		for (N_GameObject* goRef : m_goRefVector) {
+			idVector.push_back(goRef->getUUID());
+		}
+		j["goUUIDs"] = idVector;
+		j["timeStampPriority"] = m_timeStampPriority;
+		j["priority"] = m_priority;
+	}
+
+	/**
+	* Return the timestamp the N_Event should execute at
+	*/
+	int64_t N_Event::getTimeStampPriority() const {
+		return m_timeStampPriority;
+	}
+
+	/**
+	* Sets the time stamp to the specified parameter
+	*/
+	void N_Event::setTimeStampPriority(int64_t timeStampPriority) {
+		m_timeStampPriority = timeStampPriority;
+	}
+
+	/**
+	* Returns the priority of the N_Event
+	*/
+	int N_Event::getPriority() {
+		return m_priority;
+	}
+
+	/**
+	* Sets the priority of the N_Event
+	*/
+	void N_Event::setPriority(int priority) {
+		m_priority = priority;
+	}
+}
---
+++ n_event.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#ifndef N_EVENT_H
+#define N_EVENT_H
+
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+class N_GameObject;
+
+namespace N_Events {
+
+	/**
+	* Class representation of the Event that houses a reference to a GameObject (if applicable) and
+	* multiple priority fields that help to compare between Events
+	*/
+	class N_Event {
+	protected:
+
+		// Reference to the GameObject that this Event should be attached to
+		std::vector<N_GameObject*> m_goRefVector;
+		// A Timestamp value that is used to determine when the Event should be dispatched
+		int64_t m_timeStampPriority;
+		// The priority integer value used to help sort the priority queue alongside the Timestamp
+		int m_priority;
+
+	public:
+
+		N_Event();
+
+		/**
+		* Constructor for the Event struct
+		*
+		* @param goRef: A reference to the GameObject that the Event will affect
+		* @param timeStampPriority: The timestamp at which the Event should be deployed
+		* @param priority: A secondary priority
+		*/
+		N_Event(std::vector<N_GameObject*> goRef, int64_t timeStampPriority, int priority);
+
+		/**
+		* Destructor for an Event that gets rid of the reference to GameObject
+		*/
+		~N_Event();
+
+		/**
+		* A function that executes when an Event is dispatched
+		*/
+		virtual void onEvent() const = 0;
+
+		/**
+		* Returns a string in JSON format for the Event, GameObjectUUID is sent as 0 (invalid) if there is no reference
+		*/
+		virtual void to_json(json& j);
+
+		/**
+		* Return the timestamp the event should execute at
+		*/
+		int64_t getTimeStampPriority() const;
+
+		/**
+		* Sets the time stamp to the specified parameter
+		*/
+		void setTimeStampPriority(int64_t timeStampPriority);
+
+		/**
+		* Returns the priority of the Event
+		*/
+		int getPriority();
+
+		/**
+		* Sets the priority of the Event
+		*/
+		void setPriority(int priority);
+
+		/**
+		* A less than operator that can be used to compare Events to each other
+		*
+		* @param other: The other event for comparison
+		* @returns Whether or not the Event is considered less than the other based on the two priority values
+		*/
+		bool operator<(N_Event* other) const {
+			return
+				m_timeStampPriority < other->m_timeStampPriority || // Less than if timestamp is smaller than the other
+				(m_timeStampPriority == other->m_timeStampPriority && m_priority < other->m_priority); // or if the priority value is lower if the timestamp is the same
+		}
+
+		/**
+		* A less than operator that can be used to compare Events to each other
+		*
+		* @param other: The other event for comparison
+		* @returns Whether or not the Event is considered less than the other based on the two priority values
+		*/
+		bool operator>(N_Event* other) const {
+			return
+				m_timeStampPriority > other->m_timeStampPriority || // Less than if timestamp is smaller than the other
+				(m_timeStampPriority == other->m_timeStampPriority && m_priority > other->m_priority); // or if the priority value is lower if the timestamp is the same
+		}
+	};
+}
+
+#endif
